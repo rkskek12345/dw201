cc799b9799b78fe9ca6e34e7e3fceda8b86f4ffa
rkskek12345 <qsdklp1@naver.com>
2023-08-24 14:11:04 +0900

    0824

--- 2023 08 23/static/js/index1.js
+++ 2023 08 23/static/js/index1.js
@@ -1,16 +1,69 @@
-let house=new Object();
-
-$.getJSON("./data/index1.json",function(data){
-    console.log(data);
-
-});
+let house=new Object(); //전체 데이터 저장용
 
 $(function(){
-   
-    $("#icon").click(function(){
+    $.getJSON("./data/index1.json", function(data){
+        house=data; //전체 데이터 저장
+        var set = new Set(); // 분류만 골라내기 위한 set
+        $("#main").append('<div class="expenBox title"><h3>지출내역</h3>'+
+        '<span class="categoryText">분류</span>'+
+        '<span class="moneyText">금액</span>'+
+        '<span class="detailText">내용</span></div>');
+        $.each(house.지출,function(i,d){  // 지출 부분 화면 출력
+            set.add(d.분류);
+            $("#main").append('<div class="detail">'+
+            '<span class="categoryText">'+d.분류+'</span>'+
+            '<span class="moneyText">'+d.금액.toLocaleString()+'원</span>'+
+            '<span class="detailText">'+d.내용+'</span>'+
+            '</div>');
+        })
+        $("#main").append('<div class="incomeBox title"><h3>수입내역</h3>'+
+        '<span class="categoryText">분류</span>'+
+        '<span class="moneyText">금액</span>'+
+        '<span class="detailText">내용</span></div>');
+        $.each(house.수입,function(i,d){ // 수입 부분 화면 출력
+            set.add(d.분류);
+            $("#main").append('<div class="detail">'+
+            '<span class="categoryText">'+d.분류+'</span>'+
+            '<span class="moneyText">'+d.금액.toLocaleString()+'원</span>'+
+            '<span class="detailText">'+d.내용+'</span>'+
+            '</div>');
+        })
+
+        var cg = Array.from(set);  
+        $.each(cg,function(i,c){   // 분류 ul 태그 추가
+            $("#category").append('<li><input type="checkbox" name="category" value="'+c+'">'+c+'</li>')
+        });
+        
+        //console.log(cg);
+    });
+    $("#icon").click(function(){  // 사이드 접펼
         $(this).toggleClass("open");
         $("#side").toggleClass("sideopen");
+        $("#wrap").toggleClass("sideactive");
     });
 
-});
+    $("#word").on("keyup",keywordSearch);// input 입력
+    $("#money").on("keyup",moneyBelow); // 금액 입력 부분 
+})
 
+function moneyBelow(){
+    var moneyB= $(this).val();//입력한 금액 가져오기
+    if(moneyB !=''){    //금액 입력했다면
+        $(".detail").filter(function(){ //필터처리
+            var isShow=true;
+            var m = $(this).find(".moneyText").text().replace("원","").replace(/,/g,"");
+            //나는 화면에 천단위콤마 해놓아서 , 제거 하는거까지 한것
+            if( Number(moneyB) < Number(m) ) isShow=false;
+            $(this).toggle( isShow );
+        });
+    }
+}
+
+function keywordSearch(){
+    var word = $(this).val();
+    
+    $(".detail").filter(function(){
+
+        $(this).toggle( $(this).text().indexOf(word) > -1);
+    });
+}
