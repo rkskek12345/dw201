7acddc67709a49838b84f5055d7219ac8af00bef
rkskek12345 <qsdklp1@naver.com>
2023-09-05 17:27:23 +0900

    0905

---
+++ NodeJS/vuejs2/src/components/AnimalSlide.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+    msg:{ type:String, required:true },
+    info:{type:Array,required:true}
+})
+//Props type 종류 : String, Number, Array, Object, Boolean, Date, Function
+</script>
+
+<template>
+    <div class="slide">
+        <h2>{{ msg }}</h2>
+        <div class="slide_box">
+            <div class="card" v-for="a in info" :key="a">
+                <img src="">
+                <p>{{ a }}</p>
+                <p>수컷</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
